0b004dca65dac675e8fde59ef862435750ee5476
Paco Iossifov <pascal.iossifov@gmail.com>
2021-07-17 22:13:19 -0400

    added the lesson on plot function

---
+++ basics/play_with_plot.py
@@ -0,0 +1,16 @@
+from pylab import plot, show, arange
+
+plot([1,2,3,5,4],[1,1,2,39,5],'.:')
+
+def paco(x):
+  return x*x + 2*x - 3
+  
+xs = arange(-5,6,0.00001)
+ys = []
+
+for x in xs:
+  ys.append(paco(x))
+  
+plot(xs,ys,'*-')
+  
+show()
