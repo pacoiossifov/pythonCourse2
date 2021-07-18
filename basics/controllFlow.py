d373033ee77fee5cc9aab57b8e8b2aef3ba132c2
Paco Iossifov <pascal.iossifov@gmail.com>
2021-07-18 11:39:18 -0400

    check point

--- basics/controllFlow.py
+++ basics/controllFlow.py
@@ -1,1 +1,18 @@
 # this is where we will refresh the controll flow tools in python.
+from random import random
+
+a = [1,2,3,4]
+
+print(a[0],a[1],a[2],a[3], "WHO DO WE APRECIATE")
+'''
+for i in range(2):
+    r = random()
+    if random() < 0.3:
+        print("yay you were lucky!! :)")
+    elif random() > 0.3:
+        print("oof better luck next time ;) :(")
+    else:
+        print("how did you get here")
+'''
+
+print("NOT DAD")
--- basics/play_with_plot.py
+++ basics/play_with_plot.py
@@ -5,7 +5,7 @@ plot([1,2,3,5,4],[1,1,2,39,5],'.:')
 def paco(x):
   return x*x + 2*x - 3
   
-xs = arange(-5,6,0.00001)
+xs = arange(-5,6,0.001)
 ys = []
 
 for x in xs:
